553bc05a8f9fd299ddef3e6bf671429ccdb02f44
domingospawn616 <105325019+domingospawn616@users.noreply.github.com>
2022-06-05 13:39:20 -0300

    Delete webAuth01.Web directory

--- webAuth01.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace webAuth01.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
