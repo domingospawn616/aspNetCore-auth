8751421fd6acb137b96bcdda20023f17e2777052
domingospawn <domingospawn@gmail.com>
2022-06-05 13:23:15 -0300

    Configuração inicial do projeto, criado por padrão no Visual Studio e configurado para SQLite

---
+++ webAuth01.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webAuth01.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
